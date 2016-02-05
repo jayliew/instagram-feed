127d59a97d1fdd265fd8afc6aa40e8714e9e6cfb
Jay Liew <jayliew@jayliew.com>
2016-02-04 16:15:09 -0800

    added InstagramCell

---
+++ Instagram/InstagramCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstagramCell.swift
+//  Instagram
+//
+//  Created by Jay Liew on 2/4/16.
+//  Copyright © 2016 Jay Liew. All rights reserved.
+//
+
+import UIKit
+
+class InstagramCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
